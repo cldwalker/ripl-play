c1ba4ad2f30ce2d299707e8d4b0b5d0e6a919d5a
Gabriel Horner <gabriel.horner@gmail.com>
2010-11-28 12:50:37 -0500

    play from stdin

--- lib/ripl/play.rb
+++ lib/ripl/play.rb
@@ -3,11 +3,22 @@ require 'ripl'
 module Ripl::Play
   def before_loop
     super
-    play_back if File.exists? config[:play].to_s
+    play_back
   end
 
   def play_back
-    File.read(config[:play]).split("\n").each do |input|
+    if !$stdin.tty?
+      play_back_string($stdin.read)
+      $stdin.reopen '/dev/tty'
+    elsif File.exists? config[:play].to_s
+      play_back_string(File.read(config[:play]))
+    else
+      abort "ripl can't play `#{config[:play]}'"
+    end
+  end
+
+  def play_back_string(str)
+    str.split("\n").each do |input|
       puts prompt + input
       eval_input(input)
       print_result(@result)
@@ -16,3 +27,4 @@ module Ripl::Play
 end
 
 Ripl::Shell.send :include, Ripl::Play
+Ripl.config[:play] = 'ripl_tape'
