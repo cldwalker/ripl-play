5b51e59c2b29c5f6fa8d900290935054a56e0f7a
Gabriel Horner <gabriel.horner@gmail.com>
2010-12-03 10:26:01 -0500

    dont include exit words in recording

--- lib/ripl/record.rb
+++ lib/ripl/record.rb
@@ -3,7 +3,9 @@ require 'ripl'
 module Ripl::Record
   def after_loop
     super
-    saved_history = Array(history).reverse.slice(0, @line - 1).reverse
+    saved_history = Array(history)
+    saved_history.pop if Ripl::Shell::EXIT_WORDS.include?(saved_history[-1])
+    saved_history = saved_history.reverse.slice(0, @line - 1).reverse
     File.open(config[:play], 'w') {|f| f.write saved_history.join("\n") }
   end
 end
